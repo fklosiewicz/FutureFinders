501efbdcdafaf6470e71216928bb617cedba18f7
group471-admin <so305@rutgers.edu>
2022-02-16 21:15:33 -0500

    button map, thanks - Sofia

---
+++ PFF/app/src/main/java/com/example/pff/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.pff;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity<color> extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
