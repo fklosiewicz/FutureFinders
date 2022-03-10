de738d64143e7dd85d9ba745316967c2b9db9d5d
Zining Ou <zo19@rutgers.edu>
2022-03-10 17:00:33 -0500

    Resolve conflicts in ResultsActivity.java

--- PFF/app/src/main/java/com/example/pff/ResultsActivity.java
+++ PFF/app/src/main/java/com/example/pff/ResultsActivity.java
@@ -11,20 +11,21 @@ import java.util.ArrayList;
 
 
 public class ResultsActivity<color> extends AppCompatActivity {
-
-    public ArrayList<String> states;
-    public ArrayList<String> indicators;
+//    public ArrayList<String> states;
+//    public ArrayList<String> indicators;
+    public ArrayList<Integer> states;
+    public ArrayList<Integer> indicators;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.results);
 
-        states = getIntent().getExtras().getStringArrayList("States");
-        indicators = getIntent().getExtras().getStringArrayList("Indicators");
+        states = getIntent().getExtras().getIntegerArrayList("States");
+        indicators = getIntent().getExtras().getIntegerArrayList("Indicators");
 
-        ((TextView)findViewById(R.id.textView2)).setText(indicators.get(0));
-        ((TextView)findViewById(R.id.textView6)).setText(states.get(0));
+//        ((TextView)findViewById(R.id.textView2)).setText(indicators.get(0));
+//        ((TextView)findViewById(R.id.textView6)).setText(states.get(0));
 
     }
 
