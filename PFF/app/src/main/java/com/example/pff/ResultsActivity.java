968d81134d81eb52b1de1a408560580040d9b818
group471-admin <so305@rutgers.edu>
2022-03-13 12:59:29 -0500

    correct results page loads depending on if logged it or not

--- PFF/app/src/main/java/com/example/pff/ResultsActivity.java
+++ PFF/app/src/main/java/com/example/pff/ResultsActivity.java
@@ -89,11 +89,19 @@ public class ResultsActivity<color> extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.guest_results);
+
+
 
         states = getIntent().getExtras().getIntegerArrayList("States");
         indicators = getIntent().getExtras().getIntegerArrayList("Indicators");
         activeUser = getIntent().getExtras().containsKey("activeUser") ? (User) getIntent().getExtras().getSerializable("activeUser") : null;
+
+        if(activeUser == null){
+            setContentView(R.layout.guest_results);
+        }else{
+            setContentView(R.layout.results);
+        }
+
 
         LayoutInflater li = getLayoutInflater();
         View v1 = li.inflate(R.layout.activity_main, null);
@@ -153,7 +161,6 @@ public class ResultsActivity<color> extends AppCompatActivity {
                 new InfoAsyncTask().execute(s3);
                 break;
             default://not really needed because we will have 1, 2, or 3 states for sure...
-
         }
 
 
